8fefe11e2145c45cc44b50bdba3496fd1daf8f0b
Szűcs Ádám <adam@bitraptors.com>
2024-02-05 21:09:00 +0100

    add jetpack compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,5 +54,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.bom)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
 }
